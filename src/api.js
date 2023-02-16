f985ad2265282edebc134f9e9ad3f88306a9cff6
ANIRUDH-VADERA <anirudhvadera@gmail.com>
2023-02-17 04:59:39 +0530

    Deployment

--- src/api.js
+++ src/api.js
@@ -7,5 +7,5 @@ export const geoApiOptions = {
 };
 export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
 
-export const WEATHER_API_URL = "http://api.weatherapi.com/v1";
+export const WEATHER_API_URL = "https://api.weatherapi.com/v1";
 export const WEATHER_API_KEY = "8ded4cdbda0342c1a5c94159231602";
